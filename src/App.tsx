0b676536fce4f5543e9659377942ffbbe447f938
Nikolai Sundal <nikolaisundal@gmail.com>
2026-04-01 17:10:28 +0200

    changed se fasit button, and fixed bug which happened because of to main containers

--- src/App.tsx
+++ src/App.tsx
@@ -50,92 +50,93 @@ function App() {
     <>
       <div className="main">
         <div className="initial-snow">
-          <div className="initial-snow">
-            <div className="snow">🐣</div>
-            <div className="snow">🥚</div>
-            <div className="snow">🐰</div>
-            <div className="snow">⛷️</div>
-            <div className="snow">🍬</div>
-            <div className="snow">🐥</div>
-            <div className="snow">☀️</div>
-            <div className="snow">🍫</div>
-            <div className="snow">❄️</div>
-            <div className="snow">🎿</div>
-            <div className="snow">🐇</div>
-            <div className="snow">🥚</div>
-            <div className="snow">🍭</div>
-            <div className="snow">🐰</div>
-            <div className="snow">⛷️</div>
-            <div className="snow">🐤</div>
-            <div className="snow">☀️</div>
-            <div className="snow">🍫</div>
-            <div className="snow">🥚</div>
-            <div className="snow">❄️</div>
-            <div className="snow">🐣</div>
-            <div className="snow">🎿</div>
-            <div className="snow">🌷</div>
-            <div className="snow">🍬</div>
-            <div className="snow">⛷️</div>
+          <div className="snow">🐣</div>
+          <div className="snow">🥚</div>
+          <div className="snow">🐰</div>
+          <div className="snow">⛷️</div>
+          <div className="snow">🍬</div>
+          <div className="snow">🐥</div>
+          <div className="snow">☀️</div>
+          <div className="snow">🍫</div>
+          <div className="snow">❄️</div>
+          <div className="snow">🎿</div>
+          <div className="snow">🐇</div>
+          <div className="snow">🥚</div>
+          <div className="snow">🍭</div>
+          <div className="snow">🐰</div>
+          <div className="snow">⛷️</div>
+          <div className="snow">🐤</div>
+          <div className="snow">☀️</div>
+          <div className="snow">🍫</div>
+          <div className="snow">🥚</div>
+          <div className="snow">❄️</div>
+          <div className="snow">🐣</div>
+          <div className="snow">🎿</div>
+          <div className="snow">🌷</div>
+          <div className="snow">🍬</div>
+          <div className="snow">⛷️</div>
 
-            <div className="snow hidden md:block">🐣</div>
-            <div className="snow hidden md:block">🥚</div>
-            <div className="snow hidden md:block">🐰</div>
-            <div className="snow hidden md:block">🌷</div>
-            <div className="snow hidden md:block">⛷️</div>
-            <div className="snow hidden md:block">🍬</div>
-            <div className="snow hidden md:block">🐥</div>
-            <div className="snow hidden md:block">☀️</div>
-            <div className="snow hidden md:block">🍫</div>
-            <div className="snow hidden md:block">❄️</div>
-            <div className="snow hidden md:block">🎿</div>
-            <div className="snow hidden md:block">🐇</div>
-            <div className="snow hidden md:block">🥚</div>
-            <div className="snow hidden md:block">🍭</div>
-            <div className="snow hidden md:block">🐰</div>
-            <div className="snow hidden md:block">🐤</div>
-            <div className="snow hidden md:block">☀️</div>
-            <div className="snow hidden md:block">🍫</div>
-            <div className="snow hidden md:block">🥚</div>
-            <div className="snow hidden md:block">🐣</div>
-            <div className="snow hidden md:block">🎿</div>
-            <div className="snow hidden md:block">🍬</div>
-            <div className="snow hidden md:block">⛷️</div>
-            <div className="snow hidden md:block">🐇</div>
-            <div className="snow hidden md:block">🍭</div>
+          <div className="snow hidden md:block">🐣</div>
+          <div className="snow hidden md:block">🥚</div>
+          <div className="snow hidden md:block">🐰</div>
+          <div className="snow hidden md:block">🌷</div>
+          <div className="snow hidden md:block">⛷️</div>
+          <div className="snow hidden md:block">🍬</div>
+          <div className="snow hidden md:block">🐥</div>
+          <div className="snow hidden md:block">☀️</div>
+          <div className="snow hidden md:block">🍫</div>
+          <div className="snow hidden md:block">❄️</div>
+          <div className="snow hidden md:block">🎿</div>
+          <div className="snow hidden md:block">🐇</div>
+          <div className="snow hidden md:block">🥚</div>
+          <div className="snow hidden md:block">🍭</div>
+          <div className="snow hidden md:block">🐰</div>
+          <div className="snow hidden md:block">🐤</div>
+          <div className="snow hidden md:block">☀️</div>
+          <div className="snow hidden md:block">🍫</div>
+          <div className="snow hidden md:block">🥚</div>
+          <div className="snow hidden md:block">🐣</div>
+          <div className="snow hidden md:block">🎿</div>
+          <div className="snow hidden md:block">🍬</div>
+          <div className="snow hidden md:block">⛷️</div>
+          <div className="snow hidden md:block">🐇</div>
+          <div className="snow hidden md:block">🍭</div>
 
-            <div className="snow hidden xl:block">🐣</div>
-            <div className="snow hidden xl:block">🥚</div>
-            <div className="snow hidden xl:block">🐰</div>
-            <div className="snow hidden xl:block">🌷</div>
-            <div className="snow hidden xl:block">⛷️</div>
-            <div className="snow hidden xl:block">🍬</div>
-            <div className="snow hidden xl:block">🐥</div>
-            <div className="snow hidden xl:block">☀️</div>
-            <div className="snow hidden xl:block">🍫</div>
-            <div className="snow hidden xl:block">❄️</div>
-            <div className="snow hidden xl:block">🎿</div>
-            <div className="snow hidden xl:block">🐇</div>
-            <div className="snow hidden xl:block">🥚</div>
-            <div className="snow hidden xl:block">🍭</div>
-            <div className="snow hidden xl:block">🐰</div>
-            <div className="snow hidden xl:block">🐤</div>
-            <div className="snow hidden xl:block">☀️</div>
-            <div className="snow hidden xl:block">🍫</div>
-            <div className="snow hidden xl:block">🥚</div>
-            <div className="snow hidden xl:block">🐣</div>
-            <div className="snow hidden xl:block">🎿</div>
-            <div className="snow hidden xl:block">🍬</div>
-            <div className="snow hidden xl:block">⛷️</div>
-            <div className="snow hidden xl:block">🐇</div>
-            <div className="snow hidden xl:block">🍭</div>
-          </div>
+          <div className="snow hidden xl:block">🐣</div>
+          <div className="snow hidden xl:block">🥚</div>
+          <div className="snow hidden xl:block">🐰</div>
+          <div className="snow hidden xl:block">🌷</div>
+          <div className="snow hidden xl:block">⛷️</div>
+          <div className="snow hidden xl:block">🍬</div>
+          <div className="snow hidden xl:block">🐥</div>
+          <div className="snow hidden xl:block">☀️</div>
+          <div className="snow hidden xl:block">🍫</div>
+          <div className="snow hidden xl:block">❄️</div>
+          <div className="snow hidden xl:block">🎿</div>
+          <div className="snow hidden xl:block">🐇</div>
+          <div className="snow hidden xl:block">🥚</div>
+          <div className="snow hidden xl:block">🍭</div>
+          <div className="snow hidden xl:block">🐰</div>
+          <div className="snow hidden xl:block">🐤</div>
+          <div className="snow hidden xl:block">☀️</div>
+          <div className="snow hidden xl:block">🍫</div>
+          <div className="snow hidden xl:block">🥚</div>
+          <div className="snow hidden xl:block">🐣</div>
+          <div className="snow hidden xl:block">🎿</div>
+          <div className="snow hidden xl:block">🍬</div>
+          <div className="snow hidden xl:block">⛷️</div>
+          <div className="snow hidden xl:block">🐇</div>
+          <div className="snow hidden xl:block">🍭</div>
         </div>
+
         <div className="w-full h-screen fixed flex justify-center overflow-y-auto">
           <div className="w-full max-w-sm relative  pt-32">
-            <div className="flex justify-center flex-col items-center text-slate-800 mb-36 border-2 border-black rounded-full h-96 bg-white">
-              <h1 className="text-4xl font-bold  w-fit ">Nikolais</h1>
-              <div className="bg-yellow-400 h-32 w-32 rounded-full my-6"></div>
-              <h1 className="text-4xl font-bold w-fit">påskequiz</h1>
+            <div className="relative flex justify-center items-center text-slate-800 mb-36 border-2 border-black rounded-full h-72 w-72 bg-white mx-auto">
+              <div className="bg-yellow-400 h-48 w-48 rounded-full"></div>
+              <h1 className="absolute text-4xl font-bold top-24">Nikolais</h1>
+              <h1 className="absolute text-4xl font-bold bottom-24">
+                påskequiz
+              </h1>
             </div>
 
             <div className=" space-y-10 pb-32">
@@ -149,10 +150,18 @@ function App() {
                 />
               ))}
             </div>
-            <div className="w-full flex justify-center items-center h-52 pb-96">
+            <div className="w-full flex justify-center items-center h-52 pb-96 pt-24">
               {isQuizChecked ? (
-                <button className="bg-gradient-to-r from-red-100 to-green-100 p-4 border-2 border-black rounded-lg font-semibold w-72 h-20 text-2xl">
-                  {`Score: ${points} / ${questionList.length}`}
+                <button
+                  className="bg-white p-4 border-2 border-black font-semibold text-2xl space-y-4"
+                  style={{
+                    borderRadius: "50% 50% 50% 50% / 60% 60% 40% 40%",
+                    width: "160px",
+                    height: "200px",
+                  }}
+                >
+                  <div className="text-5xl">🐣</div>
+                  <div>{`${points} / ${questionList.length}`}</div>{" "}
                 </button>
               ) : (
                 <div
@@ -161,7 +170,12 @@ function App() {
                   }
                 >
                   <button
-                    className="p-4 border-2 border-black rounded-lg font-semibold hover:drop-shadow-xl hover:-translate-y-0.5 duration-100 w-72 h-20 text-2xl bg-gradient-to-r from-red-200 to-green-200"
+                    className="bg-gradient-to-r from-[#f1eeee] to-[#fbfbf5] p-4 border-2 border-black font-semibold hover:drop-shadow-xl hover:-translate-y-0.5 duration-100 text-2xl"
+                    style={{
+                      borderRadius: "50% 50% 50% 50% / 60% 60% 40% 40%",
+                      width: "160px",
+                      height: "200px",
+                    }}
                     onClick={checkAnswers}
                   >
                     Se fasit
